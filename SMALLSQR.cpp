273067da9ca48d1da04009442d4b32862bd5313b
nandahkrishna <nanda.harishankar@gmail.com>
2019-03-05 23:59:31 +0530

    Uploads

---
+++ SMALLSQR.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long l[100001];
+long long m[100001];
+int main()
+{
+    long long n, q, x, y;
+    cin>>n;
+    for(long long i = 0; i < n; i++) {
+        cin>>x>>y;
+        x = (x < 0) ? -x : x;
+        y = (y < 0) ? -y : y;
+        l[i] = max(x,y);
+    }
+    sort(l, l+n);
+    cin>>q;
+    for(long long i = 0; i < q; i++) {
+        cin>>x;
+        cout<<l[x-1]<<'\n';
+    }
+    return 0;
+}
